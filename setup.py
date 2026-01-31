ad55de975e6298e6c33ec099a2cfd6ea3f7d27e7
roboaltic <roboaltic19@gmail.com>
2026-01-31 17:46:01 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+data_files=[
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/launch',
+        ['launch/diff_drive.launch.py']),
+],
