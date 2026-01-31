747fa1f9c2032d9460515e3e0bb42c7753f76950
roboaltic <roboaltic19@gmail.com>
2026-01-31 17:49:18 +0200

    diff_drive_node.py

--- motor_node.py
+++ motor_node.py
@@ -1,49 +1,85 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-import lgpio
+from geometry_msgs.msg import Twist
+import RPi.GPIO as GPIO
 
+# ===== GPIO CONFIG =====
+# Left motor
 IN1 = 17
 IN2 = 27
+ENA = 18  # PWM
+
+# Right motor
 IN3 = 22
 IN4 = 23
+ENB = 19  # PWM
 
-class MotorNode(Node):
+PWM_FREQ = 1000
+MAX_DUTY = 100
+
+
+class DiffDriveL298N(Node):
+
     def __init__(self):
-        super().__init__('motor_node')
+        super().__init__('diff_drive_l298n')
 
-        self.sub = self.create_subscription(
-            String,
-            'motor_cmd',
-            self.cmd_callback,
+        GPIO.setmode(GPIO.BCM)
+        for pin in [IN1, IN2, ENA, IN3, IN4, ENB]:
+            GPIO.setup(pin, GPIO.OUT)
+
+        self.pwm_left = GPIO.PWM(ENA, PWM_FREQ)
+        self.pwm_right = GPIO.PWM(ENB, PWM_FREQ)
+
+        self.pwm_left.start(0)
+        self.pwm_right.start(0)
+
+        self.subscription = self.create_subscription(
+            Twist,
+            '/cmd_vel',
+            self.cmd_vel_callback,
             10
         )
 
-        self.h = lgpio.gpiochip_open(0)
-        for pin in [IN1, IN2, IN3, IN4]:
-            lgpio.gpio_claim_output(self.h, pin)
+    def set_motor(self, speed, in1, in2, pwm):
+        if speed > 0:
+            GPIO.output(in1, GPIO.HIGH)
+            GPIO.output(in2, GPIO.LOW)
+        elif speed < 0:
+            GPIO.output(in1, GPIO.LOW)
+            GPIO.output(in2, GPIO.HIGH)
+        else:
+            GPIO.output(in1, GPIO.LOW)
+            GPIO.output(in2, GPIO.LOW)
 
-        self.get_logger().info('Motor GPIO node started')
+        pwm.ChangeDutyCycle(min(abs(speed), MAX_DUTY))
 
-    def cmd_callback(self, msg):
-        cmd = msg.data
+    def cmd_vel_callback(self, msg: Twist):
+        linear = msg.linear.x
+        angular = msg.angular.z
 
-        if cmd == 'forward':
-            lgpio.gpio_write(self.h, IN1, 1)
-            lgpio.gpio_write(self.h, IN2, 0)
-            lgpio.gpio_write(self.h, IN3, 1)
-            lgpio.gpio_write(self.h, IN4, 0)
+        left = linear - angular
+        right = linear + angular
 
-        elif cmd == 'stop':
-            for pin in [IN1, IN2, IN3, IN4]:
-                lgpio.gpio_write(self.h, pin, 0)
+        left_pwm = int(left * 100)
+        right_pwm = int(right * 100)
+
+        self.set_motor(left_pwm, IN1, IN2, self.pwm_left)
+        self.set_motor(right_pwm, IN3, IN4, self.pwm_right)
+
+    def destroy_node(self):
+        self.pwm_left.stop()
+        self.pwm_right.stop()
+        GPIO.cleanup()
+        super().destroy_node()
+
 
 def main():
     rclpy.init()
-    node = MotorNode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
+    node = DiffDriveL298N()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
