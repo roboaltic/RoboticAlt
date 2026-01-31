55906206f6f5d245714b7b63006c9a1aefea4527
roboaltic <roboaltic19@gmail.com>
2026-01-31 22:05:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,10 @@ package_name = 'diff_drive_l298n'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch',
-            ['launch/diff_drive.launch.py']),
-    ],
+    packages=[diff_drive_l298n],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
+    maintainer='artem',
     description='Differential drive L298N controller',
     license='MIT',
     entry_points={
