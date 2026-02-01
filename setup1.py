c330e0533911480f6bda3e05b0636350dc1b979f
roboaltic <roboaltic19@gmail.com>
2026-02-01 22:08:07 +0200

    Create setup1.py

---
+++ setup1.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+package_name = 'camera_driver'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ahmat',
+    maintainer_email='ahmat@example.com',
+    description='ROS2 node to publish USB camera images',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'camera_node = camera_driver.camera_node:main'
+        ],
+    },
+)
