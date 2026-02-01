b6fff8bc21f361526ec4b043bb9e83d04c96e2b2
roboaltic <roboaltic19@gmail.com>
2026-02-01 02:41:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ package_name = 'diff_drive_l298n'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[diff_drive_l298n],
+    packages=[package_name],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='artem',
-    description='Differential drive L298N controller',
+    maintainer_email='artem@todo.todo',
+    description='L298N diff drive controller',
     license='MIT',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'diff_drive_node = diff_drive_l298n.diff_drive_node:main',
+            'diff_drive_node = diff_drive_l298n.motor_node:main',
         ],
     },
 )
