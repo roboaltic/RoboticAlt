6f06f10dc740ceafc320978c370ed4aba184bbbb
roboaltic <roboaltic19@gmail.com>
2026-02-03 22:24:57 +0200

    Update robot.launch.py

--- robot.launch.py
+++ robot.launch.py
@@ -1,1 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    camera_node = Node(
+        package='camera_driver',
+        executable='camera_node',
+        name='camera_node',
+        output='screen',
+        parameters=[
+            {
+                'device': '/dev/video0',
+                'fps': 15,
+                'width': 640,
+                'height': 480
+            }
+        ]
+    )
+
+    motor_node = Node(
+        package='motor_driver',
+        executable='motor_node',
+        name='motor_node',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        camera_node,
+        motor_node
+    ])
 
