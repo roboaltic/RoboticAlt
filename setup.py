b29e50404e0287c35d995c1236f2ed64becf9fb6
roboaltic <roboaltic19@gmail.com>
2026-02-05 00:06:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 from setuptools import setup
+import os
+from glob import glob
 
-package_name = 'camera_driver'
+package_name = 'robot_bringup'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')),  # <-- вот этот момент
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='artem',
-    maintainer_email='artem@example.com',
-    description='ROS2 node to publish USB camera images',
+    maintainer_email='artem@todo.todo',
+    description='Bringup package for robot (camera + motors)',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-            'camera_node = camera_driver.camera_node:main'
-        ],
+        'console_scripts': [],
     },
 )
