e813a97c09fff3982a5342d6ac68e7c48eae58bc
roboaltic <roboaltic19@gmail.com>
2026-02-14 17:11:07 +0200

    Create setup2.py

---
+++ setup/setup2.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'robot_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.py')),  # <-- вот этот момент
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='artem',
+    maintainer_email='artem@todo.todo',
+    description='Bringup package for robot (camera + motors)',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'route_executor = diff_drive_l298n.route_executor:main'
+        ],
+    },
+)
