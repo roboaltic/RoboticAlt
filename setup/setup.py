cd742297a08f1edecf154aa48f6e192d1494dc60
roboaltic <roboaltic19@gmail.com>
2026-02-14 17:10:02 +0200

    Update setup.py

--- setup/setup.py
+++ setup/setup.py
@@ -23,6 +23,8 @@ setup(
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [],
+        'console_scripts': [
+            'motor_node = diff_drive_l298n.motor_node:main'
+        ],
     },
 )
